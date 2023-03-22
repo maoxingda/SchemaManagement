231827d7b2d00bfcade1bd2266e96c3f184a343f
maoxd <maoxingda@meican.com>
2023-03-22 14:47:50 +0800

    去掉执行SQL建表功能，因为外部表建表语句不能在事务里面执行

--- SchemaGenerator/urls.py
+++ SchemaGenerator/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('dbconn/<int:dbconn_id>/generate/', views.schema_generate, name='schema_generate'),
     path('dbconn/<int:dbconn_id>/redshift/generate/', views.redshift_schema_generate, name='redshift_schema_generate'),
+    path('dbconn/<int:dbconn_id>/redshift/create-table/', views.redshift_create_table, name='redshift_create_table'),
 ]
--- SchemaGenerator/views.py
+++ SchemaGenerator/views.py
@@ -1,6 +1,7 @@
 import textwrap
 
 import psycopg2
+import sqlparse
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
@@ -30,10 +31,10 @@ def schema_generate(request, dbconn_id):
         }}
     ''')
 
-    with open('/Users/maoxd/open-source/my-terraform/locals.tf', 'w') as f:
-        f.write(locals_template)
+    # with open('/Users/maoxd/open-source/my-terraform/locals.tf', 'w') as f:
+    #     f.write(locals_template)
 
-    messages.info(request, mark_safe(f'<pre>{dbconn}: \n{locals_template}</pre>'))
+    messages.info(request, mark_safe(f'<pre>{locals_template}</pre>'))
 
     return redirect(dbconn)
 
@@ -47,17 +48,25 @@ def redshift_schema_generate(request, dbconn_id):
         table_name_prefix = f'{dbconn.target_table_name_prefix}'
     with psycopg2.connect(dw_dbconn.server_address()) as conn:
         with conn.cursor() as cursor:
+            ddl_sql = ''
             for table in dbconn.tables.all():
                 query_columns_sql = f"select column_name, data_type, character_maximum_length, numeric_precision, numeric_scale " \
                                     f"from pg_catalog.svv_all_columns where schema_name = '{dbconn.target_schema}' and table_name = '{table_name_prefix }_{table.name}'"
                 print(query_columns_sql)
                 cursor.execute(query_columns_sql)
                 columns = cursor.fetchall()
-                max_column_lengh = max([len(column) for column, _, _, _, _ in columns])
+                max_column_lengh = max([len(column) for column, _, _, _, _ in columns] + [1])
                 max_column_lengh = 25 if max_column_lengh < 25 else max_column_lengh
 
-                ddl_sql = f'create table if not exists {req_schema}.{"db_" if req_schema == "ods" else ""}{dbconn.name}_{table.name}\n' \
-                          f'(\n'
+                external = ''
+                if_not_exists = ''
+                if req_schema == 'emr':
+                    external = 'external'
+                else:
+                    if_not_exists = 'if not exists'
+
+                ddl_sql += f'create {external} table {if_not_exists} {req_schema}.{"db_" if req_schema == "ods" else ""}{dbconn.name}_{table.name}\n' \
+                           f'(\n'
                 for column, data_type, column_lengh, numeric_precision, numeric_scale in columns:
                     if data_type in ('smallint', 'integer', 'bigint', 'date', 'timestamp', 'real', 'double precision'):
                         ddl_sql += f'    {column.ljust(max_column_lengh)} {data_type},\n'
@@ -78,19 +87,38 @@ def redshift_schema_generate(request, dbconn_id):
                 ddl_sql += f"    {'cdc_transact_id'.ljust(max_column_lengh)} varchar(50),\n"
                 if req_schema == 'ods':
                     ddl_sql += f"    {'event_time'.ljust(max_column_lengh)} varchar(128),\n"
-                ddl_sql += f"    {'timestamp_executed_insert'.ljust(max_column_lengh)} timestamp default sysdate + interval '8h',\n"
-                ddl_sql += f"    {'timestamp_executed_update'.ljust(max_column_lengh)} timestamp default sysdate + interval '8h'\n"
+                    ddl_sql += f"    {'timestamp_executed_insert'.ljust(max_column_lengh)} timestamp default sysdate + interval '8h',\n"
+                    ddl_sql += f"    {'timestamp_executed_update'.ljust(max_column_lengh)} timestamp default sysdate + interval '8h'\n"
+                else:
+                    ddl_sql = ddl_sql[:-2] + '\n'
                 ddl_sql += ')\n'
                 if req_schema == 'emr':
                     ddl_sql += 'partitioned by (event_time varchar(128))\n'
                     ddl_sql += 'stored as parquet\n'
-                    ddl_sql += f"location 's3_path'\n"
-                ddl_sql += f";"
+                    ddl_sql += f"location 's3://bi-data-lake/realtime-cdc/id-mapping/id_mapping'\n"
+                ddl_sql += f";\n\n"
 
-                if settings.DEBUG:
-                    with open(f'ods.db_{dbconn.name}_{table.name}.sql', 'w') as f:
-                        f.write('\n-- 建表语句\n' + ddl_sql)
+    if settings.DEBUG:
+        with open(f'{req_schema}-create-table-ddl.sql', 'w') as f:
+            f.write('\n-- 建表语句\n' + ddl_sql)
 
-                messages.info(request, mark_safe(f'<pre>{ddl_sql}</pre>'))
+    messages.info(request, mark_safe(f'<pre>{ddl_sql}</pre>'))
+
+    return redirect(dbconn)
+
+
+def redshift_create_table(request, dbconn_id):
+    req_schema = request.GET.get('schema')
+    dbconn = get_object_or_404(DbConn, id=dbconn_id)
+    dw_dbconn = get_object_or_404(DbConn, name='beta')
+
+    with open(f'{req_schema}-create-table-ddl.sql') as f:
+        sql_stmts = f.read()
+
+    with psycopg2.connect(dw_dbconn.server_address()) as conn:
+        with conn.cursor() as cursor:
+            for sql_stmt in sqlparse.split(sql_stmts):
+                messages.info(request, mark_safe(f'<pre>{sql_stmt}</pre>'))
+                cursor.execute(sql_stmt)
 
     return redirect(dbconn)
